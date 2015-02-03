79d4df584fe75c6bbddac76568d9f4af42a7c182
Kris <kris@meridian-ds.com>
2015-02-03 00:39:12 -0600

    missed the docs on Classiphpy class itself

--- src/Classiphpy.php
+++ src/Classiphpy.php
@@ -8,11 +8,17 @@
 use Classiphpy\Output\OutputInterface;
 use Classiphpy\Processor\ProcessorInterface;
 
+/**
+ * Class Classiphpy
+ *
+ * Composes the various Classiphpy components together for ease of use.
+ */
 class Classiphpy {
 
   /**
+   * An array of DefinitionInterface implementing classes.
+   *
    * @var array
-   *   An array of DefinitionInterface implementing classes.
    */
   protected $definitionClasses;
 
@@ -22,13 +28,18 @@ class Classiphpy {
   protected $output;
 
   /**
+   * Create new instance of Classiphpy
+   *
    * @param array $definition_classes
+   *   An array of class names which implement \Classiphpy\Definition\DefintionInterface
    * @param OutputInterface $output
+   *   The OutputInterface class that will render our File(s).
    * @throws Exception
    */
   function __construct(array $definition_classes, OutputInterface $output) {
     foreach ($definition_classes as $definition_class) {
       if (!is_subclass_of($definition_class, '\Classiphpy\Definition\DefinitionInterface')) {
+        /* @todo Throw a custom exception */
         throw new \Exception(sprintf('The %s class is not an instance of \Classiphpy\Definition\DefinitionInterface', $definitionClass));
       }
     }
@@ -36,20 +47,38 @@ class Classiphpy {
     $this->output = $output;
   }
 
-  public function build($data, ProcessorInterface $processor) {
+  /**
+   * Defines, processes and writes the File(s) derived from $data.
+   *
+   * @param array $data
+   *   The data from which to generate new DefinitionInterface object(s).
+   * @param ProcessorInterface $processor
+   *   The processor to turn DefinitionInterface object(s) into File(s).
+   */
+  public function build(array $data, ProcessorInterface $processor) {
     $classes = [];
     foreach ($this->getDefinitionClasses() as $definitionClass) {
       /** @var $definitionClass \Classiphpy\Definition\DefinitionInterface */
-      $test = $definitionClass::iteratorFactory($data);
-      $classes += $test;
+      $classes += $definitionClass::iteratorFactory($data);
     }
-    return $this->getOutputMethod()->writeOut($processor->process($classes, $data));
+    $this->getOutputMethod()->writeOut($processor->process($classes, $data));
+    /* @todo determine how to define success/failure and return a boolean, or some message stack */
   }
 
+  /**
+   * Retrieve the array of definition classes.
+   *
+   * @return array
+   */
   public function getDefinitionClasses() {
     return $this->definitionClasses;
   }
 
+  /**
+   * Retrieve the OutputInterface object.
+   *
+   * @return OutputInterface
+   */
   public function getOutputMethod() {
     return $this->output;
   }
